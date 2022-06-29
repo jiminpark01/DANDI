003cf04f05697413c466bdc0d1e50d4c59c22de9
choyunju <yoonj1860@naver.com>
2022-06-30 00:56:01 +0900

    intro

---
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  //activity_main을 불러온다.
+
+    }
+}
+
